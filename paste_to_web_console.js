175d2a3141add9c9e37528f982118947a4b4b929
garywill <garywill@disroot.org>
2022-04-30 19:49:30 +0800

    加入很多

--- paste_to_web_console.js
+++ paste_to_web_console.js
@@ -1,4 +1,8 @@
+// TODO 
+// 从上到下滚一遍（让所有ajax内容和图片加载）
+// 滚回顶 （让导航栏位置正确）
 
+// ========= 删掉能删掉的脚本 ====
 var scriptTags = document.querySelectorAll("script");
 
 for (var i=scriptTags.length-1 ; i>=0 ; i--)
@@ -8,11 +12,46 @@ var allHtml = document.documentElement.innerHTML;
 document.documentElement.innerHTML = "";
 document.documentElement.innerHTML = allHtml;
 
-// document.body.innerHTML = document.body.innerHTML;
+
+//================== 让body宽度为窗口高度 ===============
 
 const winH = window.innerHeight;
-document.body.style.width = winH + "px";
+document.body.style.width = winH + "px"; 
 
+// ============= 使用旋转过cjk的字体 ======================
+
+var styleTag = document.createElement("style");
+styleTag.textContent = `
+        @font-face{
+            font-family: 'TBRL_CJK_rotate_font'; 
+            src: url('http://localhost:8899/font.php');
+        }
+        * {
+            font-family:  TBRL_CJK_rotate_font !important;
+        }
+`;
+document.head.appendChild(styleTag);
+
+// ================= 图片旋转=============
+var imgs = document.getElementsByTagName("img");
+imgs = Array.from(imgs);
+
+var alinks = document.getElementsByTagName("a");
+for (var i=0; i<alinks.length; i++) {
+    alink = alinks[i];
+    if ( getComputedStyle(alink).backgroundImage != "none" )
+        imgs.push(alink);
+}
+
+
+for (var i=0; i<imgs.length; i++) {
+    const img = imgs[i];
+    const w = parseInt(getComputedStyle(img).width);
+    const h = parseInt(getComputedStyle(img).height);
+    var scale = Math.min(w,h) / Math.max(w,h);
+    img.style.transform = ` rotate(-90deg) scale(${scale}) `;
+}
+// ============ 旋转整个网页 =======================
 var TBRL_POSITIONER = document.createElement("div"); // 用于作为body下定形的
 TBRL_POSITIONER.id = "TBRL_POSITIONER";
 
@@ -44,5 +83,20 @@ document.body.appendChild(TBRL_POSITIONER);
 
 document.body.appendChild(TBRL_OUTER);
 
+//================== 取消刚刚设置的body宽度 ===============
+
+document.body.style.width = ''; 
+// ==========================================
+styleTag.textContent += `
+    html, body {
+        overflow: auto !important;
+        overflow-y: auto !important;
+        overflow-x: auto !important;
+    }
+`
+  
+// =============== 滚到最右边=========
+window.scrollTo( 99999990, 0 );
 
 
+// TODO 鼠标滚轮互换
